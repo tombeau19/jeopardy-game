d6beeed632425de8d2885c6c52b6d8adb7dc250c
tombeau19 <thomas.beauregard@gmail.com>
2017-09-13 09:54:29 -0400

    added baseball questions, fixed multiple choice

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 $(document).ready(function () {
 
+    const baseballQuestions = [
+        {
+            question: 'This team plays at Fenway Park',
+            answer: 'Red Sox',
+            choices: 'Yankees, Red Sox, Orioles',
+            points: 100,
+        },
+        {
+            question: 'This team has won the most World Series',
+            answer: 'Yankees',
+            choices: 'Yankees, Dodgers, Giants',
+            points: 200,
+        },
+        {
+            question: 'Before Barry Bonds cheated and broke the record, This player had the most Home Runs',
+            answer: 'Hank Aaron',
+            choices: 'Babe Ruth, Roger Marris, Ted Williams, Hank Aaron',
+            points: 300,
+        },
+        
+        
+
+    ]
 
 
-    $('.baseballHundred').on('click', function () {
-        const baseballQuestion = $('<div class="btn-large disabled col s2 offset-s1">100</div>');
-        $(this).closest('.hundred').prepend(baseballQuestion);
-        $(this).remove();
-        prompt('This team plays in Boston');
+    $('.baseball').on('click', function () {
+        $(this).addClass('disabled');
+        prompt(baseballQuestions[2].question, `Type ${baseballQuestions[2].choices}`);
     });
+
+
+
+
+
+
 
 
 
